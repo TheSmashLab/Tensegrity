ad63b33215a78edb507da70d0a499172e46762b0
Caleb Swain <ccs3715@byu.edu>
2026-02-27 15:55:53 -0700

    Update __init__.py to include NonlinProps and ConnectionNonlinear in imports and __all__ list

--- TensegritySim/__init__.py
+++ TensegritySim/__init__.py
@@ -4,11 +4,13 @@ TensegritySim Package
 This package provides tools for simulating and visualizing tensegrity structures.
 """
 
-from .data_structures import Node, Connection, Surface, Tensegrity
+from .data_structures import Node, Connection, Surface, Tensegrity, NonlinProps, ConnectionNonlinear
 from .yaml_parser import YamlParser
 from .visualization import Visualization
 from .tensegrity_solver import TensegritySolver
 
-__all__ = ["Node", "Connection", "Surface", "Tensegrity", "YamlParser", "Visualization", "TensegritySolver"]
+__all__ = ["Node", "Connection", "Surface", "Tensegrity",
+           "NonlinProps", "ConnectionNonlinear",
+           "YamlParser", "Visualization", "TensegritySolver"]
 
 version = "1.0.0"
