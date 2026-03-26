3f0b2260f7a5cae6e2bddad90d1a8304a7a952e0
Caleb Swain <ccs3715@byu.edu>
2026-03-26 14:12:02 -0600

    Trying Bond's code part 2.

--- TensegritySim/tensegrity_solver.py
+++ TensegritySim/tensegrity_solver.py
@@ -70,7 +70,7 @@ class TensegritySolver:
             None. Changes are made internally to the Tensegrity object.
         """
         x0 = self._create_initial_guess() # The current positions of the nodes (excluding pinned nodes)
-
+        initial_force_norm = self._force_norm()
         result = root(self._objective, x0, method=method) # solver
 
         if not result.success:
